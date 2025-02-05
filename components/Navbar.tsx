0331c1c7dc10b55e730c8d55bccfd9e858e5eafa
Soufiane <soufiane.ait@outlook.fr>
2025-02-05 11:33:07 +0100

    setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const isProduction = process.env.NODE_ENV === "production";
+  
+  return (
+    <div className="flex justify-center items-center p-4 bg-gray-200 h-16">
+      <div className="flex items-center gap-2">
+        <h1 className="text-md font-bold">
+          L'Assiette Futée {isProduction ? "" : "(dev v1)"}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
