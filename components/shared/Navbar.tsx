a3be2095932415086781ce160065d7e5660d56c5
Soufiane <soufiane.ait@outlook.fr>
2025-02-06 12:42:32 +0100

    removing github and docker workflows

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -9,7 +9,6 @@ const Navbar = () => {
         <h1 className="text-md font-bold">
           L'Assiette Futée {isProduction ? "" : "(dev v1)"}
         </h1>
-        <p>Hello</p>
       </div>
     </div>
   );
