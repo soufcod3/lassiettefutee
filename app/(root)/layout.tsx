91e18ad6b6073dcb8f2c41d51baa52770cfc34ef
Soufiane <soufiane.ait@outlook.fr>
2025-02-08 15:20:45 +0100

    featured setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body
         className={`${mulish.className} bg-zinc-950 flex justify-center items-center h-screen text-zinc-950`}
       >
-        <div className="w-full h-screen sm:max-w-[375px] sm:h-[667px] sm:mx-auto sm:shadow-lg sm:rounded-xl bg-white overflow-hidden">
+        <div className="w-full h-screen sm:max-w-[375px] sm:h-[667px] sm:mx-auto sm:shadow-lg sm:rounded-xl bg-white overflow-scroll">
           {children}
         </div>
       </body>
--- components/home/Featured.tsx
+++ components/home/Featured.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Featured = () => {
     return (
         <div>
-            <h1>Featured</h1>
+            <h1 className="text-2xl font-bold text-center pt-12">Nos assiettes</h1>
+            <p className="text-center pt-2 px-12 text-gray-600 text-sm">Prêtes à être dégustées</p>
         </div>
     )
 }
