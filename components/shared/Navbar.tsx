44aeaaa2f018314ef22b49965772fa4bab105578
Soufiane <soufiane.ait@outlook.fr>
2025-02-17 13:01:28 +0100

    remove logs

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -32,12 +32,6 @@ const Navbar = () => {
     }
   }, [searchParams]);
 
-  useEffect(() => {
-    console.warn('NEXT_PUBLIC_ENVIRONMENT', process.env.NEXT_PUBLIC_ENVIRONMENT);
-    console.error('NEXT_PUBLIC_ENVIRONMENT', process.env.NEXT_PUBLIC_ENVIRONMENT);
-
-  }, []);
-
   return (<>
     <div className="flex justify-center items-center p-4 h-16">
       <h1 className="text-md font-bold">
